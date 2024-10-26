daa3b1b2e4315597fc94b0653db3063266dcf4ec
DDibyajyot <dibyajyotdeepam@gmail.com>
2024-10-26 10:17:07 +0100

    add google tag for analytics

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,14 @@ import { AppHeader, AppFooter, AppMetadata } from "components";
 import Loading from "./loading";
 import "styles/globals.css";
 import { ThemeContext } from "context";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata = { ...AppMetadata };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="G-FFTHTY565T" />
       <body>
         <ThemeContext>
           <AppHeader />
